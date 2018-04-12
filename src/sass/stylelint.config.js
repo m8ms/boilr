162d045975768419155139685708029aaf17f549
pm <piotr.m.martyniak@gmail.com>
2018-04-12 22:05:35 +0200

    initial set up from boilerplate

--- src/sass/stylelint.config.js
+++ src/sass/stylelint.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-    "extends": "stylelint-config-recommended-scss",
+    "extends": [
+        "stylelint-config-recommended-scss",
+        "stylelint-config-property-sort-order-smacss"
+    ],
     "plugins": [
         "stylelint-scss"
     ],
     "ignoreFiles" : [
         __dirname + "/vendor/_normalize.scss"
-    ]
-}
+    ],
+    "rules": {
+        "rule-empty-line-before": [ "always", {
+            "except": ["first-nested"],
+            "ignore": ["after-comment"]
+        } ],
+    }
+};
